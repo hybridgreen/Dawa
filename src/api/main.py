0e6d9c5c6fdf882ba2b287c29c33f85b773ffb5d
hybridgreen <y.yayaoye@gmail.com>
2026-04-01 12:37:11 +0100

    fix(deployment): Add CORD Middleware setup

--- src/api/main.py
+++ src/api/main.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
 from src.lib.gemini import gemini_ai
@@ -11,13 +11,6 @@ from src.config import config as cfg
 from src.lib.hybrid_search import HybridSearch
 from src.lib.medicine_data import process_all_pdfs, load_cached_docs
 
-class ChatForm(BaseModel):
-    query: str
-    limit: int = 5
-    therapeutic_area: str | None = None
-    active_substance: str | None = None
-    atc_code: str | None = None
-    
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     
@@ -32,25 +25,59 @@ async def lifespan(app: FastAPI):
         )
     
     print("Loading search engine...")
+    
     app.state.search_engine = HybridSearch(documents=documents, model_name=cfg.model)
+    
     print("✓ Ready")
     
     yield 
     
     print("Shutting down...")
 
+
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost",      
+    "http://localhost:8080",
+    "https://dawa-one.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 gemini = gemini_ai("gemini-2.5-flash")
 
 
+class ChatForm(BaseModel):
+    query: str
+    limit: int = 5
+    therapeutic_area: str | None = None
+    active_substance: str | None = None
+    atc_code: str | None = None
+    
+    
 @app.get("/")
 def root():
     return {"message":"Welcome to Dawa"}
 
+@app.get("/healthz/")
+def health_check():
+    if not app.state.search_engine:
+        raise HTTPException(status_code= 500, detail= "Unable to initialise search engine") 
+    
+    return "Healthy"
+
 @app.post("/chats/")
 def message( data: ChatForm
 ):
+    if data.query.strip() == "":
+        raise HTTPException(status_code=400, detail= "Empty query, please type an question")
     
     query = gemini.spell(data.query)
     
